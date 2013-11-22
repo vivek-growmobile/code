2404b34e9e3edda32f653828d6189fed14155e62
Vivek Sivakumar <vivek@growmobile.com>
2013-11-22 11:05:42 -0800

    set timeout in web server

--- web-server.js
+++ web-server.js
@@ -2,7 +2,10 @@ var http = require('http');
 
 var s = http.createServer(function(req, res){
 	res.writeHead(200, {'content-type': 'text/plain'});
-	res.end("hello world\n");
+	res.write("hello\n");
+	setTimeout(function(){
+		res.end("world\n");
+	    }, 2000);
     });
 
 s.listen(8000);
